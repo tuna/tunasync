4b046aa7408227ecf4600dfb779d5284cdcf3d5c
bigeagle <justin.w.xd@gmail.com>
2015-02-13 18:50:01 +0800

    nodesource

---
+++ scripts/nodesource.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+function sync_repo_ck() {
+        repo_url="$1"
+        repo_dir="$2"
+
+        [ ! -d "$repo_dir" ] && mkdir -p "$repo_dir"
+        cd $repo_dir
+        lftp "${repo_url}/" -e "mirror --verbose  -P 5 --delete --only-newer; bye"
+}
+
+sync_repo_ck "https://deb.nodesource.com/node" "${TUNASYNC_WORKING_DIR}/deb"
+sync_repo_ck "https://rpm.nodesource.com/pub" "${TUNASYNC_WORKING_DIR}/rpm"
