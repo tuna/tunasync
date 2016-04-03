7da4dfc78e7f517dc06a81452ce9767fc27d7b69
bigeagle <justin.w.xd@gmail.com>
2016-04-03 19:19:53 +0800

    update lxc syncing script

--- scripts/lxc-images.sh
+++ scripts/lxc-images.sh
@@ -3,11 +3,14 @@
 function sync_lxc_images() {
 	repo_url="$1"
 	repo_dir="$2"
+
+	[ ! -d "$repo_dir" ] && mkdir -p "$repo_dir"
 	cd $repo_dir
 
 	# lftp "${repo_url}/" -e "mirror --verbose --log=${TUNASYNC_LOG_FILE} --exclude-glob='*/SRPMS/*' -P 5 --delete --only-newer; bye"
-	lftp "${repo_url}/" -e "mirror --verbose --exclude lxd/ -P 5 --delete --only-newer; bye"
+	lftp "${repo_url}/" -e "mirror --verbose -P 5 --delete --only-newer; bye"
 }
 
 
-sync_lxc_images "http://images.linuxcontainers.org/" "${TUNASYNC_WORKING_DIR}/"
+sync_lxc_images "http://images.linuxcontainers.org/images" "${TUNASYNC_WORKING_DIR}/images"
+sync_lxc_images "http://images.linuxcontainers.org/meta" "${TUNASYNC_WORKING_DIR}/meta"
