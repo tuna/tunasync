84b7bdd713cd76f85b331c97df547b78d758d90c
bigeagle <justin.w.xd@gmail.com>
2016-04-28 19:37:46 +0800

    tests: self-signed TLS certificates

---
+++ tests/httpClient.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/tuna/tunasync/internal"
+)
+
+func main() {
+	cfg, err := internal.GetTLSConfig("rootCA.crt")
+	fmt.Println(err)
+	var msg map[string]string
+	resp, err := internal.GetJSON("https://localhost:5002/", &msg, cfg)
+	fmt.Println(err)
+	fmt.Println(resp)
+}
---
+++ tests/httpServer.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	s := gin.Default()
+	s.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"msg": "passed"})
+	})
+	s.RunTLS(":5002", "manager.crt", "manager.key")
+}
