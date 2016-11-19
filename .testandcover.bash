e37bb44fa3c519c558127816a744489984edfa5f
bigeagle <justin.w.xd@gmail.com>
2016-11-20 00:51:20 +0800

    ci(tunasync): upload to github releases

--- .testandcover.bash
+++ .testandcover.bash
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-
 function die() {
   echo $*
   exit 1
@@ -8,7 +6,7 @@ function die() {
 
 export GOPATH=`pwd`:$GOPATH
 
-make
+make travis
 
 # Initialize profile.cov
 echo "mode: count" > profile.cov
@@ -19,10 +17,9 @@ ERROR=""
 # Test each package and append coverage profile info to profile.cov
 for pkg in `cat .testpackages.txt`
 do
-    #$HOME/gopath/bin/
-    go test -v -covermode=count -coverprofile=profile_tmp.cov $pkg || ERROR="Error testing $pkg"
-
-    [ -f profile_tmp.cov ] && {
+	go test -v -covermode=count -coverprofile=profile_tmp.cov $pkg || ERROR="Error testing $pkg"
+	
+	[ -f profile_tmp.cov ] && {
 		tail -n +2 profile_tmp.cov >> profile.cov || die "Unable to append coverage for $pkg"
 	}
 done
