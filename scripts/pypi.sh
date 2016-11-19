03d22b7683f644b986902a669a343bb8d99dafcf
bigeagle <justin.w.xd@gmail.com>
2016-11-20 01:10:24 +0800

    refactor(removing unneeded script files):

--- scripts/pypi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ ! -d "$TUNASYNC_WORKING_DIR" ]; then
-	echo "Directory not exists, fail"
-	exit 1	
-fi
-
-echo "Syncing to $TUNASYNC_WORKING_DIR"
-
-/usr/bin/timeout -s INT 3600 /home/tuna/.virtualenvs/bandersnatch/bin/bandersnatch -c /etc/bandersnatch.conf mirror || exit 1
